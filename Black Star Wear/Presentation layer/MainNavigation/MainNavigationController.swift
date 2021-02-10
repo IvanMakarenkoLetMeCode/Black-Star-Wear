f0eea9d1a82d3f8c7d6d90dc056b5023223e53b5
Ivan Makarenko <ivan.makarenko@letmecode.org>
2021-02-10 15:49:49 +0300

    feat(main): created navigationController.

---
+++ Black Star Wear/Presentation layer/MainNavigation/MainNavigationController.swift
@@ -0,0 +1,17 @@
+//
+//  MainNavigationController.swift
+//  Black Star Wear
+//
+//  Created by Ivan on 10.02.2021.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
