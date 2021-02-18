ad49515f3df982f64b40f0487eab103c55ed2ae3
Ivan Makarenko <ivan.makarenko@letmecode.org>
2021-02-18 17:51:05 +0300

    feat(extentions, customView): created vButton customView and added new functional for string extentions

--- Black Star Wear/Extentions/String.swift
+++ Black Star Wear/Extentions/String.swift
@@ -14,4 +14,38 @@ extension String {
         return NSLocalizedString(self, comment: comment)
     }
     
+    func priceFormate() -> String {
+        
+        var formatedPrice = self
+        let priceNumber = NumberFormatter().number(from: formatedPrice)
+        let formater = NumberFormatter()
+        formater.numberStyle = .currency
+        formater.locale = Locale(identifier: "ru_RU")
+        formatedPrice = formater.string(from: priceNumber ?? 0) ?? ""
+        return formatedPrice
+        
+    }
+    
+    func convertHTMLStringToAttributed(fontName: String? = nil,
+                                       fontSize: CGFloat? = nil) -> NSMutableAttributedString? {
+        var htmlString = self
+        if let fontName = fontName, let fontSize = fontSize {
+            htmlString = String(
+                format: "<span style=\"font-family: '\(fontName)'; font-size: \(fontSize)\">%@</span>",
+                self
+            )
+        }
+        let options = [
+            NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html,
+            NSAttributedString.DocumentReadingOptionKey.characterEncoding: String.Encoding.utf8.rawValue
+            ] as [NSAttributedString.DocumentReadingOptionKey: Any]
+        if let htmlData = htmlString.data(using: .unicode, allowLossyConversion: true) {
+            let attributedString = try? NSMutableAttributedString(data: htmlData,
+                                                                  options: options,
+                                                                  documentAttributes: nil)
+            return attributedString
+        }
+        return nil
+    }
+    
 }
